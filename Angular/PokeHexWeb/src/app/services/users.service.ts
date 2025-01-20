b00e6fdec51e4ae30a8364ae2d35a3cca06596c4
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-20 19:08:19 +0100

    IMPLEMENT TEST: should create a new user

--- Angular/PokeHexWeb/src/app/services/users.service.ts
+++ Angular/PokeHexWeb/src/app/services/users.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { catchError, Observable, of } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
@@ -10,5 +11,14 @@ export class UserService {
     private Url = 'http://172.21.46.184:3000/api/v1/users'; 
 
     constructor(private http: HttpClient) { }
+
+    postUser(user: any): Observable<any> {
+        return this.http.post<any>(`${this.Url}/createUser/`, user).pipe(
+            catchError((err) => {
+                console.error('Error adding user:', err);
+                return of(null)
+            })
+        )
+    }
 
 }
