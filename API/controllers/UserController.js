c89d2840ade33a27b48ae15e8f76909554651eb2
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-20 18:22:09 +0100

    Do it so it's encrypted

--- API/controllers/UserController.js
+++ API/controllers/UserController.js
@@ -1,20 +1,27 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import sql from "mssql";
 import config from "../config/dbConfig.js";
 
 export const CreateUser = async (req, res) => {
   const { name, email, type, password } = req.body;
   try {
+    //Haseja la contrasenya abans del guardarla
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     let pool = await sql.connect(config);
     let result = await pool
       .request()
       .input("name", sql.VarChar, name)
       .input("email", sql.VarChar, email)
       .input("type", sql.VarChar, type)
-      .input("password", sql.VarChar, password)
+      .input("password", sql.VarChar, hashedPassword) //Utiliza la contrasenya hasejada
       .query(
         "use PokeHexDatabase INSERT INTO Users (name, email, type, password) VALUES (@name, @email, @type, @password)"
       );
     if (result.rowsAffected[0] === 1) {
+      //Generar el token JWT
+      const token = jwt.sign({ email}, process.env.JWT_SECRET, {expiresIn: "2h"});
       return res.status(201).json({ message: "User created successfully" });
     } else {
       return res.status(400).json({ message: "Error creating user" });
