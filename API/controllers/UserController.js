7c6a7384bb51a931b265ecc01ebc899ef7299178
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-20 18:14:20 +0100

    Updeate POST CreateUser, add column password

--- API/controllers/UserController.js
+++ API/controllers/UserController.js
@@ -2,16 +2,17 @@ import sql from "mssql";
 import config from "../config/dbConfig.js";
 
 export const CreateUser = async (req, res) => {
-  const { name, email, type } = req.body;
+  const { name, email, type, password } = req.body;
   try {
     let pool = await sql.connect(config);
     let result = await pool
       .request()
-      .input("name", sql.NVarChar, name)
-      .input("email", sql.NVarChar, email)
-      .input("type", sql.NVarChar, type)
+      .input("name", sql.VarChar, name)
+      .input("email", sql.VarChar, email)
+      .input("type", sql.VarChar, type)
+      .input("password", sql.VarChar, password)
       .query(
-        "use PokeHexDatabase INSERT INTO Users (name, email, type) VALUES (@name, @email, @type)"
+        "use PokeHexDatabase INSERT INTO Users (name, email, type, password) VALUES (@name, @email, @type, @password)"
       );
     if (result.rowsAffected[0] === 1) {
       return res.status(201).json({ message: "User created successfully" });
--- sql.sql
+++ sql.sql
@@ -9,5 +9,6 @@ Create Table Users (
 	name Varchar(50) Not null,
 	email Varchar(50) Not Null,
 	type Varchar(50) Not Null,
+	password Varchar(50) Not Null,
 	Primary key (email)
 );
