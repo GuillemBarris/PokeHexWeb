02955658da5932ee12e984b7d85e79ad9c88ceca
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-20 16:43:53 +0100

    SQL:  Create Database and Create Table Users

---
+++ sql.sql
@@ -0,0 +1,13 @@
+--Creacio de la Base de Dades--
+
+Create Database PokeHexDatabase;
+
+---Creacio de les Taules---
+use PokeHexDatabase;
+
+Create Table Users (
+	name Varchar(50) Not null,
+	email Varchar(50) Not Null,
+	type Varchar(50) Not Null,
+	Primary key (email)
+);
