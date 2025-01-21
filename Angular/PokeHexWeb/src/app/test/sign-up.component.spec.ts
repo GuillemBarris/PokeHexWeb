04347bb8e5f2cb619fc14edf1ad92b2a7d818d05
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-21 15:32:51 +0100

    TEST: should have a form with name email, and password fields

--- Angular/PokeHexWeb/src/app/test/sign-up.component.spec.ts
+++ Angular/PokeHexWeb/src/app/test/sign-up.component.spec.ts
@@ -20,4 +20,14 @@ describe('SignUpComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`should have a form with name email, and password fields`, ()=>{
+    const nameInput = fixture.nativeElement.querySelector('input[name="name"]');
+    const emailInput = fixture.nativeElement.querySelector('input[name="email"]');
+    const passwordInput = fixture.nativeElement.querySelector('input[name="password"]');
+    expect(nameInput).toBeTruthy();
+    expect(emailInput).toBeTruthy();
+    expect(passwordInput).toBeTruthy();
+  });
+
 });
