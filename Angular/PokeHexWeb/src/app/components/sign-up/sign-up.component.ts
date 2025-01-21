3601c797c2e473f7a7fb16a0f715a6a4559109f3
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-21 16:39:30 +0100

    IMPLEMENT TEST: should validate name correctly

--- Angular/PokeHexWeb/src/app/components/sign-up/sign-up.component.ts
+++ Angular/PokeHexWeb/src/app/components/sign-up/sign-up.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  name: string = '';
+  errorMessage: string | null = null;
+  
+  validateName():boolean {
 
+    //Verifica que el nom no estigui buit
+    if(!this.name){
+      return false;
+    }
+
+    //Verifica que el nom no excedeixi a els 50 caracters
+    if(/^\d+$/.test(this.name)){
+      return false;
+    }
+
+    //Verifica que el nom no excedixi els 50 caracters
+    if(this.name.length > 50){
+      return false;
+    }
+
+    return true;
+  }
 
 
 }
