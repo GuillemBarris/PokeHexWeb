17357258261ea9578cbaacebd6eebdb7c2917a16
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-21 16:56:30 +0100

    IMPLEMENT TEST: should set errorMessage when name is invalid

--- Angular/PokeHexWeb/src/app/components/sign-up/sign-up.component.ts
+++ Angular/PokeHexWeb/src/app/components/sign-up/sign-up.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
@@ -16,16 +17,19 @@ export class SignUpComponent {
 
     //Verifica que el nom no estigui buit
     if(!this.name){
+      this.errorMessage = 'Name cannot be empty';
       return false;
     }
 
     //Verifica que el nom no excedeixi a els 50 caracters
     if(/^\d+$/.test(this.name)){
+      this.errorMessage = 'Name cannot contain only numbers';
       return false;
     }
 
     //Verifica que el nom no excedixi els 50 caracters
     if(this.name.length > 50){
+      this.errorMessage = 'Name cannot exceed 50 characters';
       return false;
     }
 
