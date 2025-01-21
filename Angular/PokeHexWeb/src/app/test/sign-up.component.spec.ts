6cb1fec94360cd98721f948a83db971bb72c6078
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-21 17:32:33 +0100

    TEST: should validate password correctly

--- Angular/PokeHexWeb/src/app/test/sign-up.component.spec.ts
+++ Angular/PokeHexWeb/src/app/test/sign-up.component.spec.ts
@@ -121,4 +121,30 @@ describe('SignUpComponent', () => {
     component.validateEmail();
     expect(component.errorMessage).toBe('Email format is invalid');
   });
+
+  it('should validate password correctly', () => {
+    component.password = 'ValidPass123!';
+    expect(component.validatePassword()).toBeTrue();
+
+    component.password = '';
+    expect(component.validatePassword()).toBeFalse();
+
+    component.password = 'short';
+    expect(component.validatePassword()).toBeFalse();
+
+    component.password = 'nouppercase123!';
+    expect(component.validatePassword()).toBeFalse();
+
+    component.password = 'NOLOWERCASE123!';
+    expect(component.validatePassword()).toBeFalse();
+
+    component.password = 'NoSpecialChar123';
+    expect(component.validatePassword()).toBeFalse();
+
+    component.password = 'NoNumber!';
+    expect(component.validatePassword()).toBeFalse();
+
+    component.password = 'a'.repeat(256);
+    expect(component.validatePassword()).toBeFalse();
+  });
 });
