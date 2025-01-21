5bea8e347e2adc6452a47ec378490fdde864948f
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-21 17:18:42 +0100

    IMPLEMENT TEST: should validate email correctly

--- Angular/PokeHexWeb/src/app/components/sign-up/sign-up.component.ts
+++ Angular/PokeHexWeb/src/app/components/sign-up/sign-up.component.ts
@@ -7,28 +7,28 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
   name: string = '';
+  email: string = '';
   errorMessage: string | null = null;
-  
-  validateName():boolean {
 
+  validateName(): boolean {
     //Verifica que el nom no estigui buit
-    if(!this.name){
+    if (!this.name) {
       this.errorMessage = 'Name cannot be empty';
       return false;
     }
 
     //Verifica que el nom no excedeixi a els 50 caracters
-    if(/^\d+$/.test(this.name)){
+    if (/^\d+$/.test(this.name)) {
       this.errorMessage = 'Name cannot contain only numbers';
       return false;
     }
 
     //Verifica que el nom no excedixi els 50 caracters
-    if(this.name.length > 50){
+    if (this.name.length > 50) {
       this.errorMessage = 'Name cannot exceed 50 characters';
       return false;
     }
@@ -36,5 +36,23 @@ export class SignUpComponent {
     return true;
   }
 
+  validateEmail(): boolean {
+    const trimmedEmail = this.email.trim();
 
+    if (!trimmedEmail) {
+      return false;
+    }
+
+    if (trimmedEmail.length > 50) {
+      return false;
+    }
+
+    if (
+      !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(trimmedEmail)
+    ) {
+      return false;
+    }
+
+    return true;
+  }
 }
