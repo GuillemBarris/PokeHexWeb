3380ea1e54b38bb09db3ed1e801188098c453a9c
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-22 18:19:30 +0100

    IMPLEMENT TEST: should set errorMessage when email is invalid in Login component.

--- Angular/PokeHexWeb/src/app/components/login/login.component.ts
+++ Angular/PokeHexWeb/src/app/components/login/login.component.ts
@@ -10,21 +10,25 @@ import { FormsModule } from '@angular/forms';
 })
 export class LoginComponent {
   email: string = '';
+  errorMessage: string | null = null;
 
   validateEmail(): boolean {
     const trimmedEmail = this.email.trim();
 
     if (!trimmedEmail) {
+      this.errorMessage= 'Worng email';
       return false;
     }
 
     if (trimmedEmail.length > 50) {
+      this.errorMessage= 'Worng email';
       return false;
     }
 
     if (
       !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(trimmedEmail)
     ) {
+      this.errorMessage= 'Worng email';
       return false;
     }
 
