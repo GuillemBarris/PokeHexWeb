7d49c4ca1df98cca4f52a9dc61eac01587014bac
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-22 17:52:25 +0100

    IMPLEMENT TEST: should validate email correctly in Login component

--- Angular/PokeHexWeb/src/app/components/login/login.component.ts
+++ Angular/PokeHexWeb/src/app/components/login/login.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  email: string = '';
 
+  validateEmail(): boolean {
+    const trimmedEmail = this.email.trim();
+
+    if (!trimmedEmail) {
+      return false;
+    }
+
+    if (trimmedEmail.length > 50) {
+      return false;
+    }
+
+    if (
+      !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(trimmedEmail)
+    ) {
+      return false;
+    }
+
+    return true;
+  }
 }
