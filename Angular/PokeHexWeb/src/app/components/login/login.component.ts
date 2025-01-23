6f3349fce9eeffd21570bc68635be573981fadf1
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-23 16:38:18 +0100

    IMPLEMENT TEST: should call login when the button is double-clicked and all fields are valid

--- Angular/PokeHexWeb/src/app/components/login/login.component.ts
+++ Angular/PokeHexWeb/src/app/components/login/login.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { UserService } from '../../services/users.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
@@ -12,6 +14,34 @@ export class LoginComponent {
   email: string = '';
   password: string = '';
   errorMessage: string | null = null;
+
+  constructor(private userService: UserService) {}
+
+  login() {
+    if (!this.validateEmail() || !this.validatePassword()) {
+      return;
+    } else if (
+      this.validateEmail() == false ||
+      this.validatePassword() == false
+    ) {
+      return;
+    } else if (
+      this.validateEmail() == true &&
+      this.validatePassword() == true
+    ) {
+      this.errorMessage = 'Login success';
+      this.userService
+        .getUserByEmailAndPassword(this.email, this.password)
+        .subscribe(
+          (data) => {
+            console.log('User added:', data);
+          },
+          (error) => {
+            console.error('Error adding user:', error);
+          }
+        );
+    }
+  }
 
   validateEmail(): boolean {
     const trimmedEmail = this.email.trim();
