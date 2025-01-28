32d715173c93c919a3188ac06365ccfaa1bb0541
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-28 17:13:13 +0100

    TEST: should navigate to /admin-create-pokemon when goToCreatePokemon is called

--- Angular/PokeHexWeb/src/app/test/admin-home.component.spec.ts
+++ Angular/PokeHexWeb/src/app/test/admin-home.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminHomeComponent } from '../components/admin-home/admin-home.component';
+import { Router } from '@angular/router';
 
 describe('AdminHomeComponent', () => {
   let component: AdminHomeComponent;
-  let fixture: ComponentFixture<AdminHomeComponent>;
+  let fixture: ComponentFixture<AdminHomeComponent>;7
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +16,23 @@ describe('AdminHomeComponent', () => {
 
     fixture = TestBed.createComponent(AdminHomeComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should navigate to /admin-create-pokemon when goToCreatePokemon is called', () => {
+    spyOn(router, 'navigate'); 
+  
+    const buttons = fixture.nativeElement.querySelectorAll('button');
+    const loginButton = buttons[0];
+    const event = new MouseEvent('dblclick');
+    loginButton.dispatchEvent(event);
+  
+    fixture.detectChanges();
+  
+    expect(router.navigate).toHaveBeenCalledWith(['/admin-create-pokemon']);
   });
 });
