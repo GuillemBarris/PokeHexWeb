9fc8213c3801d9570928c19443279f3a71dbd9d6
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-29 16:58:41 +0100

    IMPLEMENT TEST: should validate Pokemon name

--- Angular/PokeHexWeb/src/app/components/admin-create-pokemon/admin-create-pokemon.component.ts
+++ Angular/PokeHexWeb/src/app/components/admin-create-pokemon/admin-create-pokemon.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-create-pokemon',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './admin-create-pokemon.component.html',
   styleUrl: './admin-create-pokemon.component.css'
 })
 export class AdminCreatePokemonComponent {
+
+  pokemonName: string = '';
+
+  validPokemonName(): boolean {
+    const trimmedPokemonName = this.pokemonName.trim();
+
+    if (!trimmedPokemonName) {
+      return false;
+    }
+
+    if (trimmedPokemonName.length >= 15) {
+      return false;
+    }
+
+    if (!/^[a-zA-Z0-9]*$/.test(trimmedPokemonName)) {
+      return false;
+    }
+
+    return true;
+  }
 
 }
