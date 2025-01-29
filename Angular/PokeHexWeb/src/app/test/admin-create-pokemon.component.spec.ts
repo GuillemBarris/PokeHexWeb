0defa6f50bc8bc841fa4e5e1ae9be7220b7a56d5
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-29 17:35:53 +0100

    TEST: should validate Pokemon generation, category, and stats

--- Angular/PokeHexWeb/src/app/test/admin-create-pokemon.component.spec.ts
+++ Angular/PokeHexWeb/src/app/test/admin-create-pokemon.component.spec.ts
@@ -48,6 +48,103 @@ describe('AdminCreatePokemonComponent', () => {
     expect(component.errorMessage).toBe('Worng Pokemon name. Pokemon name cannot be longer than 15 characters');
   });
 
+  it('should validate Pokemon generation, category, and stats', () => {
+    // Validate Pokemon generation
+    component.pokemonGeneration = 1;
+    expect(component.validPokemonGeneration()).toBeTrue();
 
+    component.pokemonGeneration = 0;
+    expect(component.validPokemonGeneration()).toBeFalse();
 
+    component.pokemonGeneration = '9'.repeat(2);
+    expect(component.validPokemonGeneration()).toBeFalse();
+
+    // Validate Pokemon category
+    component.pokemonCategory = 'Electric';
+    expect(component.validPokemonCategory()).toBeTrue();
+
+    component.pokemonCategory = '';
+    expect(component.validPokemonCategory()).toBeFalse();
+
+    component.pokemonCategory = 'Electric'.repeat(26);
+    expect(component.validPokemonCategory()).toBeFalse();
+
+    component pokemonCategory = 'Electric!@';
+    expect(component.validPokemonCategory()).toBeFalse();
+    // Validate Pokemon Ps
+    component.pokemonPs = 35;
+   
+    expect(component.validPokemonPs()).toBeTrue();
+
+    component.pokemonPs = -1;
+    expect(component.validPokemonPs()).toBeFalse();
+
+    component.pokemonPS = 256;
+    expect(component.validPokemonPs()).toBeFalse();
+
+    //Validate Pokemon Attack
+
+    component.pokemonAttack = 55;
+
+    expect(component.validPokemonAttack()).toBeTrue();
+
+    component.pokemonAttack = -1;
+    expect(component.validPokemonAttack()).toBeFalse();
+
+    component.pokemonAttack = 256;
+
+    expect(component.validPokemonAttack()).toBeFalse();
+
+    //Validate Pokemon Defense
+
+    component.pokemonDefense = 40;
+
+    expect(component.validPokemonDefense()).toBeTrue();
+
+    component.pokemonDefense = -1;
+    expect(component.validPokemonDefense()).toBeFalse();
+
+    component.pokemonDefense = 256;
+
+    expect(component.validPokemonDefense()).toBeFalse();
+
+    //Validate Pokemon SpAttack
+
+    component.pokemonSpAttack = 50;
+
+    expect(component.validPokemonSpAttack()).toBeTrue();
+
+    component.pokemonSpAttack = -1;
+    expect(component.validPokemonSpAttack()).toBeFalse();
+
+    component.pokemonSpAttack = 256;
+
+    expect(component.validPokemonSpAttack()).toBeFalse();
+
+    //Validate Pokemon SpDefense
+
+    component.pokemonSpDefense = 50;
+
+    expect(component.validPokemonSpDefense()).toBeTrue();
+
+    component.pokemonSpDefense = -1;
+    expect(component.validPokemonSpDefense()).toBeFalse();
+
+    component.pokemonSpDefense = 256;
+
+    expect(component.validPokemonSpDefense()).toBeFalse();
+
+    //Validate Pokemon Speed
+
+    component.pokemonSpeed = 90;
+
+    expect(component.validPokemonSpeed()).toBeTrue();
+
+    component.pokemonSpeed = -1;
+    expect(component.validPokemonSpeed()).toBeFalse();
+
+    component.pokemonSpeed = 256;
+
+    expect(component.validPokemonSpeed()).toBeFalse();
+  });
 });
