c200a8f2a95bd12b9605b80b8b504900cac07b73
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-29 20:21:59 +0100

    IMPLEMENT TEST: http://172.24.59.209:3000/api/v1/users

--- Angular/PokeHexWeb/src/app/services/pokemon.service.ts
+++ Angular/PokeHexWeb/src/app/services/pokemon.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { catchError, of } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class PokemonService {
+
+    private Url = 'http://172.24.59.209:3000/api/v1/pokemons';
+
+    constructor(private http: HttpClient) { }
+
+    postPokemon(pokemon: any) {
+        return this.http.post<any>(`${this.Url}/createPokemon/`, pokemon).pipe(
+            catchError((err) => {
+                console.error('Error adding pokemon:', err);
+                return of(null)
+            })
+        )
+    }
+
 
 }
