600dadfe2d7df21159063ef7dc436ea08e59a82a
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-29 20:18:09 +0100

    TEST: should create a new pokemon

--- Angular/PokeHexWeb/src/app/test/services/pokemon.service.spec.ts
+++ Angular/PokeHexWeb/src/app/test/services/pokemon.service.spec.ts
@@ -22,4 +22,23 @@ describe('DatabaseService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should create a new pokemon', () => {
+    const newPokemon = {
+      name: 'name',
+      generation: 1,
+      category: 'category',
+      ps: 100,
+      attack: 100,
+      defense: 100,
+      spAttack: 100,
+      spDefense: 100,
+      speed: 100,
+    };
+    service.postPokemon(newPokemon).subscribe((response) => {
+      expect(response).toEqual(newPokemon);
+    });
+    const req = httpMock.expectOne(`${service['Url']}/createPokemon/`);
+    expect(req.request.method).toBe('POST');
+  });
 });
