39a23c07d537b7959d6e0e8cde9c51d6fe49d779
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-01-30 19:15:56 +0100

    Put Admin-Pokemon test in test carpet

--- Angular/PokeHexWeb/src/app/test/admin-pokemon.component.spec.ts
+++ Angular/PokeHexWeb/src/app/test/admin-pokemon.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AdminPokemonComponent } from './admin-pokemon.component';
-
+import { AdminPokemonComponent } from '../components/admin-pokemon/admin-pokemon.component';
 describe('AdminPokemonComponent', () => {
   let component: AdminPokemonComponent;
   let fixture: ComponentFixture<AdminPokemonComponent>;
