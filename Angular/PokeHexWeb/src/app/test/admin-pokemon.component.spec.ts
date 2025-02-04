2b4641a174b892792a4d78125972bbaa06301a9f
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-02-04 16:07:42 +0100

    TEST: should load pokemon on init

--- Angular/PokeHexWeb/src/app/test/admin-pokemon.component.spec.ts
+++ Angular/PokeHexWeb/src/app/test/admin-pokemon.component.spec.ts
@@ -1,29 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AdminPokemonComponent } from '../components/admin-pokemon/admin-pokemon.component';
-import { By } from '@angular/platform-browser';
 import { PokemonService } from '../services/pokemon.service';
 import { of } from 'rxjs';
+import { provideHttpClient } from '@angular/common/http';
+
 describe('AdminPokemonComponent', () => {
   let component: AdminPokemonComponent;
   let fixture: ComponentFixture<AdminPokemonComponent>;
   let pokemonService: jasmine.SpyObj<PokemonService>;
+
   const mockPokemons = [
-    {name: 'Pikachu', generation: 1, category: 'Pikachu', ps: 35, attack: 55, defense: 40, spAttack: 50, spDefense: 50, speed: 90},
-    {name: 'Bulbasaur', generation: 1, category: 'Seed', ps: 45, attack: 49, defense: 49, spAttack: 65, spDefense: 65, speed: 45},
-  ]
- 
+    { name: 'Pikachu', generation: 1, category: 'Pikachu', ps: 35, attack: 55, defense: 40, spAttack: 50, spDefense: 50, speed: 90 },
+    { name: 'Bulbasaur', generation: 1, category: 'Seed', ps: 45, attack: 49, defense: 49, spAttack: 65, spDefense: 65, speed: 45 },
+  ];
+
   beforeEach(async () => {
+    const pokemonServiceSpy = jasmine.createSpyObj('PokemonService', ['getPokemons']);
+
     await TestBed.configureTestingModule({
-      imports: [AdminPokemonComponent]
-    })
-    .compileComponents();
+      imports: [AdminPokemonComponent],
+      providers: [
+        provideHttpClient(),
+        { provide: PokemonService, useValue: pokemonServiceSpy } // Provide the mock service
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AdminPokemonComponent);
     component = fixture.componentInstance;
     pokemonService = TestBed.inject(PokemonService) as jasmine.SpyObj<PokemonService>;
-    fixture.detectChanges();
-    
   });
 
   it('should create', () => {
@@ -33,10 +37,9 @@ describe('AdminPokemonComponent', () => {
   it('should load pokemon on init', () => {
     pokemonService.getPokemons.and.returnValue(of(mockPokemons));
     component.ngOnInit();
+
     expect(pokemonService.getPokemons).toHaveBeenCalled();
+
     expect(component.pokemons).toEqual(mockPokemons);
-  })
-
- 
+  });
 });
-
