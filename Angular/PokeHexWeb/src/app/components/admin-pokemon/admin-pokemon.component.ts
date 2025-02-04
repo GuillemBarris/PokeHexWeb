4a084da5f9b0de056860218beb55aa1939387e71
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-02-04 16:16:10 +0100

    IMPLEMENT TEST: should load pokemon on init

--- Angular/PokeHexWeb/src/app/components/admin-pokemon/admin-pokemon.component.ts
+++ Angular/PokeHexWeb/src/app/components/admin-pokemon/admin-pokemon.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { PokemonService } from '../../services/pokemon.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-pokemon',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-pokemon.component.html',
   styleUrl: './admin-pokemon.component.css'
 })
 export class AdminPokemonComponent {
+
+  pokemons: any[] = [];
+
+  constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit(): void {
+    this.pokemonService.getPokemons().subscribe(pokemons => {
+      this.pokemons = pokemons;
+    });
+  }
+
 
 }
