51bb3263feea6e7971ebe1f8bb28bc3f6b959e3b
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-02-10 18:55:02 +0100

    POST:  CreateGame

--- API/controllers/GameController.js
+++ API/controllers/GameController.js
@@ -18,3 +18,24 @@ export const GetGameByUserId = async(req, res) => {
     }
 
 }
+
+export const CreateGame = async(req, res) => {
+    const {name, user_id} = req.body
+
+    try {
+        let pool = await sql.connect(config);
+        let result = await pool
+        .request()
+        .input("name", sql.VarChar, name)
+        .input("user_id", sql.VarChar, user_id)
+        .query("use PokeHexDatabase Insert Into Games (name, user_id) values (@name, @user_id)");
+        if(result.rowsAffected[0] === 1){
+            return res.status(201).json({message: "Game created succesfully"})
+        } else {
+            return res.status(400).json({message: "Error creating Game"})
+        }
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
--- API/routes/GameRoutes.js
+++ API/routes/GameRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { GetGameByUserId } from "../controllers/GameController.js";
+import { CreateGame, GetGameByUserId } from "../controllers/GameController.js";
 
 const GameRoutes = Router();
 
 GameRoutes.get("/getGameByUserId/:user_id", GetGameByUserId)
+GameRoutes.post("/createGame", CreateGame)
 
 export default  GameRoutes;
