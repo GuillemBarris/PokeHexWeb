1e76edbf211d4fc09d92079bce656f3a0cac145e
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-02-11 16:32:04 +0100

    IMPLEMENT TEST: should get games by email using GET request

--- Angular/PokeHexWeb/src/app/services/game.service.ts
+++ Angular/PokeHexWeb/src/app/services/game.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
-
+import { HttpClient } from "@angular/common/http";
+import { catchError, of } from "rxjs";
 @Injectable({
     providedIn:'root'
 })
@@ -7,5 +8,16 @@ import { Injectable } from "@angular/core";
 export class GameService {
 
    
+    private Url = 'http://172.24.59.209:3000/api/games';
+
+    constructor(private http: HttpClient) { }
+    getGames(email: string){
+        return this.http.get<any>(`${this.Url}/GetGameByUserId/${email}`).pipe(
+            catchError((err) =>{
+                  console.error('Error getting pokemons:', err);
+                  return of(null)
+            } )
+        )
+    }
 
 }
