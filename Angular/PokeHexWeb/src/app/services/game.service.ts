50dfca8f0f645ab9a901b012faad3292f64da3c7
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-02-11 20:59:30 +0100

    Manage service errors related to the token

--- Angular/PokeHexWeb/src/app/services/game.service.ts
+++ Angular/PokeHexWeb/src/app/services/game.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { catchError, of } from "rxjs";
+import { Router } from "@angular/router";
 @Injectable({
     providedIn:'root'
 })
@@ -10,7 +11,8 @@ export class GameService {
    
     private Url = 'http://172.24.59.209:3000/api/games';
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private router:Router) { }
+
     getGames(email: string){
         const token = localStorage.getItem('authToken'); 
         const headers = new HttpHeaders({
@@ -19,9 +21,22 @@ export class GameService {
         });
         return this.http.get<any>(`${this.Url}/GetGameByUserId/${email}`, {headers}).pipe(
             catchError((err) =>{
-                  console.error('Error getting pokemons:', err);
+                if (err.status === 400) {
+                    console.error('Error 400: Bad Request');
+                }else if (err.status === 401) {
+                    console.error('Error 401: Unauthorized Token');
+                    this.router.navigate(['/login'])
+                } else if (err.status === 403) {
+                    console.error('Error 403: Forbidden Token');
+                    this.router.navigate(['/login'])
+                }else if (err.status === 500) {
+                    console.error('Error 500: Internal Server Error');
+
+                } else {
+                    console.error('Error desconocido:', err.status);
+                }
                   return of(null)
-                  
+
             } )
         )
     }
--- Angular/PokeHexWeb/src/app/services/pokemon.service.ts
+++ Angular/PokeHexWeb/src/app/services/pokemon.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { catchError, of } from "rxjs";
 
 @Injectable({
@@ -10,7 +11,7 @@ export class PokemonService {
 
     private Url = 'http://172.24.59.209:3000/api/v1/pokemons';
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private router: Router) { }
 
     postPokemon(pokemon: any) {
         const token = localStorage.getItem('authToken'); 
@@ -21,6 +22,23 @@ export class PokemonService {
         return this.http.post<any>(`${this.Url}/createPokemon/`, pokemon, {headers}).pipe(
             catchError((err) => {
                 console.error('Error adding pokemon:', err);
+                
+                if (err.status === 400) {
+                    console.error('Error 400: Bad Request');
+                }else if (err.status === 401) {
+                    console.error('Error 401: Unauthorized Token');
+                    this.router.navigate(['/login'])
+                } else if (err.status === 403) {
+                    console.error('Error 403: Forbidden Token');
+                    this.router.navigate(['/login'])
+                }else if (err.status === 500) {
+                    console.error('Error 500: Internal Server Error');
+
+                } else {
+                    console.error('Error desconocido:', err.status);
+                }
+                   
+
                 return of(null)
             })
         )
@@ -36,7 +54,20 @@ export class PokemonService {
 
         return this.http.get<any>(`${this.Url}/getPokemons/${number}`, { headers }).pipe(
             catchError((err) => {
-                console.error('Error getting pokemons:', err);
+                if (err.status === 400) {
+                    console.error('Error 400: Bad Request');
+                }else if (err.status === 401) {
+                    console.error('Error 401: Unauthorized Token');
+                    this.router.navigate(['/login'])
+                } else if (err.status === 403) {
+                    console.error('Error 403: Forbidden Token');
+                    this.router.navigate(['/login'])
+                }else if (err.status === 500) {
+                    console.error('Error 500: Internal Server Error');
+
+                } else {
+                    console.error('Error desconocido:', err.status);
+                }
                 return of(null); 
             })
         );
