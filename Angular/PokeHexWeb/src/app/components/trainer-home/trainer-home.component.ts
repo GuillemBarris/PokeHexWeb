ccde6b3df4eef45ed845d30ded1f2671091f29e7
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-02-12 18:07:32 +0100

    Make that user's games visible in the html

--- Angular/PokeHexWeb/src/app/components/trainer-home/trainer-home.component.ts
+++ Angular/PokeHexWeb/src/app/components/trainer-home/trainer-home.component.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 import { Token } from '../../services/token.service';
 import { GameService } from '../../services/game.service';
 import { catchError, of, tap } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-trainer-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './trainer-home.component.html',
   styleUrl: './trainer-home.component.css',
 })
 export class TrainerHomeComponent {
+
+  game: any[] = [];
   UserId = localStorage.getItem('email');
 
   constructor(private token: Token, private gameService: GameService) {}
@@ -19,15 +22,11 @@ export class TrainerHomeComponent {
     this.token.TokenPresent(); 
   
     if (this.UserId) {
-      this.gameService.getGames(this.UserId).pipe(
-        tap((data) => {
-          console.log('Datos recibidos:', data);
-        }),
-        catchError((error) => {
-          console.error('Error al obtener los juegos:', error);
-          return of(null);
-        })
-      ).subscribe()
+      this.gameService.getGames(this.UserId).subscribe(games => {
+        this.game = games;
+       
+      })
+     
     }
   }
 }
