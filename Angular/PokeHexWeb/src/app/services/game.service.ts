506403677ab1afdec4a9839569ed6e56e40cd9e0
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-02-18 19:45:26 +0100

    Create Function putGame

--- Angular/PokeHexWeb/src/app/services/game.service.ts
+++ Angular/PokeHexWeb/src/app/services/game.service.ts
@@ -65,4 +65,33 @@ export class GameService {
         })
       );
   }
+  
+  putGame(gameId: string, game: any) {
+    const token = localStorage.getItem('authToken');
+    const headers = new HttpHeaders({
+      authorization: `Bearer ${token}`,
+    });
+    return this.http
+      .put<any>(`${this.Url}/updateGame/${gameId}`, game, { headers })
+      .pipe(
+        catchError((err) => {
+          console.error('Error updating game:', err);
+
+          if (err.status === 400) {
+            console.error('Error 400: Bad Request');
+          } else if (err.status === 401) {
+            console.error('Error 401: Unauthorized Token');
+            this.router.navigate(['/login']);
+          } else if (err.status === 403) {
+            console.error('Error 403: Forbidden Token');
+            this.router.navigate(['/login']);
+          } else if (err.status === 500) {
+            console.error('Error 500: Internal Server Error');
+          } else {
+            console.error('Error desconocido:', err.status);
+          }
+          return of(null);
+        })
+      );
+  }
 }
