ca8d6c05696bf23f8e0551c8a95546cdce24f723
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-02-19 18:01:36 +0100

    TEST: should update the game correctly

--- Angular/PokeHexWeb/src/app/test/trainer-home.component.spec.ts
+++ Angular/PokeHexWeb/src/app/test/trainer-home.component.spec.ts
@@ -1,26 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TrainerHomeComponent } from '../components/trainer-home/trainer-home.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { GameService } from '../services/game.service';
+import { of, throwError } from 'rxjs';
 
 describe('TrainerHomeComponent', () => {
   let component: TrainerHomeComponent;
   let fixture: ComponentFixture<TrainerHomeComponent>;
+  let gameService: GameService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TrainerHomeComponent],
-      providers:[provideHttpClient(), provideHttpClientTesting()]
-    })
-    .compileComponents();
+      providers: [provideHttpClient(), provideHttpClientTesting(), GameService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TrainerHomeComponent);
     component = fixture.componentInstance;
+    gameService = TestBed.inject(GameService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update the game correctly', () => {
+    const mockGame = { id: '1', name: 'Pokemon', user_id: '1' };
+    const mockResponse = { id: '1', name: 'Pokemon' };
+
+    spyOn(gameService, 'putGame').and.returnValue(of(mockResponse));
+    spyOn(console, 'log');
+    spyOn(console, 'error');
+
+    component.updateGame(mockGame);
+
+    expect(gameService.putGame).toHaveBeenCalledWith(mockGame.id, mockGame);
+    expect(console.log).toHaveBeenCalledWith('Game updated:', mockResponse);
+    expect(console.error).not.toHaveBeenCalled();
   });
 });
