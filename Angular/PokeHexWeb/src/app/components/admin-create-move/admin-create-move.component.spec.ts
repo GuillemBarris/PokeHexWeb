c011cfe7aea28bc5cf116cea3ae8979736c19bf5
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-02-20 17:37:13 +0100

    Create Component admin-create-move

---
+++ Angular/PokeHexWeb/src/app/components/admin-create-move/admin-create-move.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminCreateMoveComponent } from './admin-create-move.component';
+
+describe('AdminCreateMoveComponent', () => {
+  let component: AdminCreateMoveComponent;
+  let fixture: ComponentFixture<AdminCreateMoveComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AdminCreateMoveComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AdminCreateMoveComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Angular/PokeHexWeb/src/app/components/admin-create-move/admin-create-move.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-create-move',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-create-move.component.html',
+  styleUrl: './admin-create-move.component.css'
+})
+export class AdminCreateMoveComponent {
+
+}
