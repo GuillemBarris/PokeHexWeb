153a4ac8e2fd7cf73601e5ef081df37b9665c8f0
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-02-21 18:05:27 +0100

    Create a function validMoveName()

--- Angular/PokeHexWeb/src/app/components/admin-create-move/admin-create-move.component.ts
+++ Angular/PokeHexWeb/src/app/components/admin-create-move/admin-create-move.component.ts
@@ -10,4 +10,25 @@ import { FormsModule } from '@angular/forms';
 })
 export class AdminCreateMoveComponent {
   moveName: string = '';
+  errorMessage: string | null = null;
+  validMoveName(): boolean {
+    const trimmedMoveName = this.moveName.trim();
+
+    if (!trimmedMoveName) {
+      this.errorMessage = 'Worng Move name. Move name cannot be empty';
+      return false;
+    }
+
+    if (trimmedMoveName.length >= 20) {
+      this.errorMessage = 'Worng Move name. Move name cannot be longer than 15 characters';
+      return false;
+    }
+
+    if (!/^[a-zA-Z0-9]*$/.test(trimmedMoveName)) {
+      this.errorMessage = 'Worng Move name. Move name cannot contain special characters';
+      return false;
+    }
+
+    return true;
+  }
 }
