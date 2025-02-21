5138c46767de8478e8fa8e15d0e16276c96f34a1
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-02-21 17:58:17 +0100

    Agafar el moveName del formulari

--- Angular/PokeHexWeb/src/app/components/admin-create-move/admin-create-move.component.ts
+++ Angular/PokeHexWeb/src/app/components/admin-create-move/admin-create-move.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-create-move',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './admin-create-move.component.html',
   styleUrl: './admin-create-move.component.css'
 })
 export class AdminCreateMoveComponent {
-
+  moveName: string = '';
 }
