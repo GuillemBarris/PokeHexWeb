0419f13c8397b71bd94b52815fd0cd6c965eb559
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-02-25 18:50:50 +0100

    Crate Function postPokemonGame in pokemongame.service

--- Angular/PokeHexWeb/src/app/services/pokemongame.servie.ts
+++ Angular/PokeHexWeb/src/app/services/pokemongame.servie.ts
@@ -1,9 +1,44 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class PokemonGameService {
-    
+  private Url = 'http://172.24.59.209:3000/api/v1/pokemonGame';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  postPokemonGame(pokemonGame: any) {
+    const token = localStorage.getItem('authToken');
+    const headers = new HttpHeaders({
+      authorization: `Bearer ${token}`,
+    });
+    return this.http
+      .post<any>(`${this.Url}/createPokemonGame/`, pokemonGame, { headers })
+      .pipe(
+        catchError((err) => {
+          console.error('Error adding pokemon game:', err);
+
+          if (err.status === 400) {
+            console.error('Error 400: Bad Request');
+          } else if (err.status === 401) {
+            console.error('Error 401: Unauthorized Token');
+            this.router.navigate(['/login']);
+          } else if (err.status === 403) {
+            console.error('Error 403: Forbidden Token');
+            this.router.navigate(['/login']);
+          } else if (err.status === 500) {
+            console.error('Error 500: Internal Server Error');
+          } else {
+            console.error('Unknown error:', err.status);
+          }
+
+          return of(null);
+        })
+      );
+  }
 }
