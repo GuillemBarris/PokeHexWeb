7139daed17cc6ae2af8f62d54e03924f71cdb732
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-02-25 16:55:38 +0100

    PUT:  Create Route updatePokemonGame

--- API/controllers/PokemonGameController.js
+++ API/controllers/PokemonGameController.js
@@ -30,3 +30,18 @@ export const createPokemonGame = async (req, res) => {
     }
 };
 
+export const updatePokemonGame = async (req, res) => {
+    try {
+        let pool = await sql.connect(config);
+        let result = await pool.request()
+            .input('id', sql.Int, req.params.id)
+            .input('pokemon_id', sql.Int, req.body.pokemon_id)
+            .input('game_id', sql.Int, req.body.game_id)
+            .input('box_name', sql.NVarChar, req.body.box_name)
+            .input('location_in_box', sql.NVarChar, req.body.location_in_box)
+            .query("UPDATE PokemonGames SET pokemon_id = @pokemon_id, game_id = @game_id, box_name = @box_name, location_in_box = @location_in_box WHERE id = @id");
+        res.send(result);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+};
