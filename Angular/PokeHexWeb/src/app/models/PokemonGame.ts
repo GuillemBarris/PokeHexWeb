1d33fcf2a5d120cfd0a378a47704ec7b1cea0c80
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-02-25 20:33:31 +0100

    Change PokemonGame Model because pokemon_id is undefined

--- Angular/PokeHexWeb/src/app/models/PokemonGame.ts
+++ Angular/PokeHexWeb/src/app/models/PokemonGame.ts
@@ -1,13 +1,15 @@
 export class PokemonGame {
-    id: number;
-    name: string;
-    generation: number;
-    region: string;
+    id: string | undefined;
+    pokemon_id: string | undefined;
+    game_id: string;
+    box_number: number;
+    location_in_box: number;
 
-    constructor(id: number, name: string, generation: number, region: string) {
+    constructor(id: string | undefined, pokemon_id: string | undefined, game_id: string, box_number: number, location_in_box: number) {
         this.id = id;
-        this.name = name;
-        this.generation = generation;
-        this.region = region;
+        this.pokemon_id = pokemon_id;
+        this.game_id = game_id;
+        this.box_number = box_number;
+        this.location_in_box = location_in_box;
     }
 }
