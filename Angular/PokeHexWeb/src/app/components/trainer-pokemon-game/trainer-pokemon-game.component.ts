b405d5309ce4f4f614a5113dcb28b715f0202f73
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-02-26 17:18:45 +0100

    IMPLEMENT TEST: Should have gameId as the page id

--- Angular/PokeHexWeb/src/app/components/trainer-pokemon-game/trainer-pokemon-game.component.ts
+++ Angular/PokeHexWeb/src/app/components/trainer-pokemon-game/trainer-pokemon-game.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-trainer-pokemon-game',
@@ -7,6 +9,14 @@ import { Component } from '@angular/core';
   templateUrl: './trainer-pokemon-game.component.html',
   styleUrl: './trainer-pokemon-game.component.css'
 })
-export class TrainerPokemonGameComponent {
+export class TrainerPokemonGameComponent implements OnInit {
+  gameId: string | null = null;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.gameId = params.get('gameId');
+    });
+  }
 }
