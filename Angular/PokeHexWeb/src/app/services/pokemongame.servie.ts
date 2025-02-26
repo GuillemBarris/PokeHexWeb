b3c0b35e5ded70dba6724586f78d0457eab31c91
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-02-26 17:44:56 +0100

    Create function getPokemonGame in service pokemongame.service

--- Angular/PokeHexWeb/src/app/services/pokemongame.servie.ts
+++ Angular/PokeHexWeb/src/app/services/pokemongame.servie.ts
@@ -41,4 +41,34 @@ export class PokemonGameService {
         })
       );
   }
+
+  getPokemonGame(gameId: string, boxNumber: number) {
+    const token = localStorage.getItem('authToken');
+    const headers = new HttpHeaders({
+      authorization: `Bearer ${token}`,
+    });
+    return this.http
+      .get<any>(`${this.Url}/getPokemonGame${gameId}?boxNumber=${boxNumber}`, { headers })
+      .pipe(
+        catchError((err) => {
+          console.error('Error fetching pokemon game:', err);
+
+          if (err.status === 400) {
+            console.error('Error 400: Bad Request');
+          } else if (err.status === 401) {
+            console.error('Error 401: Unauthorized Token');
+            this.router.navigate(['/login']);
+          } else if (err.status === 403) {
+            console.error('Error 403: Forbidden Token');
+            this.router.navigate(['/login']);
+          } else if (err.status === 500) {
+            console.error('Error 500: Internal Server Error');
+          } else {
+            console.error('Unknown error:', err.status);
+          }
+
+          return of(null);
+        })
+      );
+  }
 }
