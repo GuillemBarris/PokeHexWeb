9d591d580c768905d2cecd4fe8d22897e324ada7
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-03-05 11:22:13 +0100

    Creat Component Trainer Pokemon Form

---
+++ Angular/PokeHexWeb/src/app/components/trainger-pokemon-form/trainger-pokemon-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trainger-pokemon-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './trainger-pokemon-form.component.html',
+  styleUrl: './trainger-pokemon-form.component.css'
+})
+export class TraingerPokemonFormComponent {
+
+}
