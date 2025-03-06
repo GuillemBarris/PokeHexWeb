63005d30fef80e25d8ef22dc3aeb0a78aa8c7bdd
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-03-06 18:13:58 +0100

    Service PokemonMoveController, GetallMoves

--- Angular/PokeHexWeb/src/app/services/move.service.ts
+++ Angular/PokeHexWeb/src/app/services/move.service.ts
@@ -39,4 +39,29 @@ export class MoveService {
         })
       );
   }
+  getAllMoves() {
+    const token = localStorage.getItem('authToken'); 
+    const headers = new HttpHeaders({
+        'authorization': `Bearer ${token}`,
+    });
+
+    return this.http.get<any>(`${this.Url}/getAllMoves/`, { headers }).pipe(
+        catchError((err) => {
+            if (err.status === 400) {
+                console.error('Error 400: Bad Request');
+            } else if (err.status === 401) {
+                console.error('Error 401: Unauthorized Token');
+                this.router.navigate(['/login']);
+            } else if (err.status === 403) {
+                console.error('Error 403: Forbidden Token');
+                this.router.navigate(['/login']);
+            } else if (err.status === 500) {
+                console.error('Error 500: Internal Server Error');
+            } else {
+                console.error('Error desconocido:', err.status);
+            }
+            return of(null);
+        })
+    );
+}
 }
