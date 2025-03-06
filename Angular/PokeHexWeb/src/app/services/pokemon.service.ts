3aa3e8e0420c9b03f682127b61c39b7f2b52708b
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2025-03-06 16:04:59 +0100

    in pokemon service get All Pokemon

--- Angular/PokeHexWeb/src/app/services/pokemon.service.ts
+++ Angular/PokeHexWeb/src/app/services/pokemon.service.ts
@@ -73,5 +73,30 @@ export class PokemonService {
         );
     }
 
+    getAllPokemons() {
+        const token = localStorage.getItem('authToken'); 
+        const headers = new HttpHeaders({
+            'authorization': `Beare ${token}`,
+        });
+
+        return this.http.get<any>(`${this.Url}/getAllPokemons`, { headers }).pipe(
+            catchError((err) => {
+                if (err.status === 400) {
+                    console.error('Error 400: Bad Request');
+                } else if (err.status === 401) {
+                    console.error('Error 401: Unauthorized Token');
+                    this.router.navigate(['/login']);
+                } else if (err.status === 403) {
+                    console.error('Error 403: Forbidden Token');
+                    this.router.navigate(['/login']);
+                } else if (err.status === 500) {
+                    console.error('Error 500: Internal Server Error');
+                } else {
+                    console.error('Error desconocido:', err.status);
+                }
+                return of(null);
+            })
+        );
+    }
 
 }
